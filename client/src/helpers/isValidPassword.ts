d8956da4686822bc876fb994b3fa30e921dd7345
Niqckow <nemarrejean9@gmail.com>
2025-11-26 15:29:36 +0100

    :recycle: refacto isValidPassword to return specific error

--- client/src/helpers/isValidPassword.ts
+++ client/src/helpers/isValidPassword.ts
@@ -1,9 +1,12 @@
-export const isValidPassword = (password: string, minLength = 8): boolean => {
-  if (password.length < minLength) return false;
+export const isValidPassword = (password: string, minLength = 8): string | boolean => {
+  if (password.length < minLength) return `Le mot de passe doit contenir au moins ${minLength} caractères.`;
 
   const hasUppercase = /[A-Z]/.test(password);
+  if (!hasUppercase) return "Le mot de passe doit contenir au moins une lettre majuscule.";
   const hasNumber = /[0-9]/.test(password);
+  if (!hasNumber) return "Le mot de passe doit contenir au moins un chiffre.";
   const hasSpecialChar = /[!@#$%^&*()_+\-=[\]{}|;:',.<>/?~]/.test(password);
+  if (!hasSpecialChar) return "Le mot de passe doit contenir au moins un caractère spécial.";
 
-  return hasUppercase && hasNumber && hasSpecialChar;
+  return true;
 };
